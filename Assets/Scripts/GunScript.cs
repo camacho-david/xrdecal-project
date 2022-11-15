5f43394a520f466e1f86965bd411b8cc59221955
Ryan Ji <ryanji7171@gmail.com>
2022-11-14 17:23:29 -0800

    Testing raycasting and haptic feedback

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -7,15 +7,14 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class GunScript : MonoBehaviour
 {
     Rigidbody rb;
-    public Transform controller;
+    public Transform controllerTransform;
     [Range(0.0f, 360.0f)]
     public float rotateBy = 0f;
-    
 
-    public GameObject FPSCam;
-    public GameObject targetObject;
+    public InputActionReference controllerActionTrigger;
 
-    [SerializeField] public InputActionReference controllerActionTrigger;
+    public XRBaseController controller;
+    public Transform raycastOrigin;
 
     // Start is called before the first frame update
     void Start()
@@ -26,39 +25,29 @@ public class GunScript : MonoBehaviour
     // Update is called once per physics update
     void FixedUpdate()
     {
-        rb.MovePosition(controller.position);
-        rb.MoveRotation(controller.rotation * Quaternion.Euler(rotateBy, 0, 0));
+        rb.MovePosition(controllerTransform.position);
+        rb.MoveRotation(controllerTransform.rotation * Quaternion.Euler(rotateBy, 0, 0));
     }
 
     // Update is called once per frame
     private void Update()
     {
-        //find out how to check for vr input to call firegun
-        /*if (Input.GetButtonDown("FireGun"))
-        {
-            fireLaserGun();
-            laserLineRenderer.enabled = true;
-            Debug.Log("firing laser gun");
-        }
-        else
-        {
-            laserLineRenderer.enabled = false;
-        }*/
-
         if (controllerActionTrigger.action.ReadValue<float>() != 0)
         {
-            Debug.Log("Trigger from " + this.gameObject.name.ToString());
+            Debug.Log("Trigger from " + gameObject.name.ToString());
+            fireGun();
         }
     }
 
     // FireGun is called when gun is fired
     void fireGun()
     {
+        controller.SendHapticImpulse(0.5f, 0.1f);
+        
         RaycastHit hit;
-        Vector3 origin = FPSCam.transform.position;
-        Vector3 direction = FPSCam.transform.forward;
-        if (Physics.Raycast(origin, direction, out hit))
+        if (Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hit))
         {
+            Debug.Log("Raycast was fired");
             GameObject hitObject = hit.collider.gameObject;
             if (hitObject.tag == "Tag of target")
             {
