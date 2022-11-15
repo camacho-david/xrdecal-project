45ee822b1b660e525ae820c583d35afe4333afdb
HSWWWW <Helena1980938619@gmail.com>
2022-11-14 18:20:27 -0800

    Note Creation with Approach animation, shatter, disappear

--- Assets/Scripts/NoteCreation.cs
+++ Assets/Scripts/NoteCreation.cs
@@ -6,36 +6,32 @@ public class NoteCreation : MonoBehaviour
 {
     public GameObject prefab;
     //stores time that a note appear
-    List<float> nums = new List<float>() { 1f, 2f, 3f, 4f, 5f };
+    List<float> nums = new List<float>() { 60/82f, 60/82f, 60/82f, 60/82f, 60/82f };
     // stores position of a time
-    List<Vector3> listOfPosition = new List<Vector3>();
-
+    List<Vector3> listOfPosition = new List<Vector3>() {};
     // Start is called before the first frame update
-    IEnumerator Start()
+    private float nextSpawn = 0.0f;
+    void Start()
     {
-        yield return StartCoroutine("CreateNote");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Time.time >= nextSpawn){
+
         
-    }
-    IEnumerator CreateNote(){
-        print("function start");
-        //create each note
-        for (var i = 0; i < nums.Count; i++) {
-            print("creating note "+ i);
-            //randomizing position
+            // //randomizing position
             var position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
             listOfPosition.Add(position);
             //wait for this many seconds to create new note
-            yield return new WaitForSeconds(i);
             GameObject go;
             print("instantiating");
-            go = Instantiate(prefab, listOfPosition[i], Quaternion.identity);
+            go = Instantiate(prefab, position, Quaternion.identity);
             //destroy note after 2 seconds
-            Object.Destroy(go, 2.0f);
+            Object.Destroy(go, 600/82f);
+            nextSpawn = Time.time+30/82f;
         }
     }
+
 }
